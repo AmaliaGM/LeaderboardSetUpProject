beae256be3fdcda28a7571b7f76502be66e714c1
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-08-03 16:17:43 -0300

    readme and commented lines

--- src/index.js
+++ src/index.js
@@ -11,18 +11,6 @@ export default function addElements() {
   name.appendChild(t);
   name.appendChild(s);
   nameList.appendChild(name);
-
-  /*          if (name !== undefined) {
-         name = JSON.stringify(inputName, null, 2);
-         score = JSON.stringify(inputScore, null, 2);
-    }
-        document.getElementsByClassName('nameInput').value;
-        document.getElementsByClassName('scoreInput').values;
- */
-  /* for (let i = 0; i < scoreList.length; i += 1) {
-          console.log(scoreList[i]);
-        }
- */
 }
 
 const addButt = document.getElementById('submit');
